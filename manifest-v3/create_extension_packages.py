898ab1e4dfd77b891bdaf9a72f9e393af019af1a
Beth <130214958+bethylamine@users.noreply.github.com>
2023-05-22 17:00:45 -0700

    Add script to create the packaged version of the extension for Chrome and Firefox.

---
+++ manifest-v3/create_extension_packages.py
@@ -0,0 +1,21 @@
+import glob, os, shutil, sys
+
+all_files = glob.glob("*")
+for file in all_files:
+	if file == sys.argv[0]:
+		all_files.remove(file)
+	if file.endswith(".zip"):
+		all_files.remove(file)
+	if file.startswith("extension-"):
+		all_files.remove(file)
+
+shutil.copytree(".", "extension-chrome")
+os.remove(os.path.join("extension-chrome", "manifest-firefox.json"))
+shutil.make_archive(base_name="soupcan-chrome", format="zip", root_dir="extension-chrome")
+shutil.rmtree("extension-chrome")
+
+shutil.copytree(".", "extension-firefox")
+os.remove(os.path.join("extension-firefox", "manifest.json"))
+os.rename(os.path.join("extension-firefox", "manifest-firefox.json"), os.path.join("extension-firefox", "manifest.json"))
+shutil.make_archive(base_name="soupcan-ff", format="zip", root_dir="extension-firefox")
+shutil.rmtree("extension-firefox")
