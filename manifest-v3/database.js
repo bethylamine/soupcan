ffc683e861b38c99f1e2f88db7e3257a63065623
Beth <130214958+bethylamine@users.noreply.github.com>
2024-04-08 15:38:31 -0700

    Closes #125 - regression

--- manifest-v3/database.js
+++ manifest-v3/database.js
@@ -17,3 +17,62 @@ function initDatabase() {
     }
   });
 }
+
+function hash(string) {
+  const utf8 = new TextEncoder().encode(string);
+  return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray
+        .map((bytes) => bytes.toString(16).padStart(2, '0'))
+        .join('');
+  });
+}
+async function getDatabaseEntry(identifier) {
+  const hashedIdentifier = await hash(identifier.toLowerCase() + ":" + database["salt"]);
+
+  const databaseEntry = database["entries"][hashedIdentifier];
+  let localEntry = localEntries[hashedIdentifier];
+  let finalEntry = databaseEntry;
+
+  if (localEntry) {
+    // Treat local entries with detected reason as nonexistent.
+    if (localEntry["reason"] === "Detected by Shinigami Eyes") {
+      localEntry = null;
+    }
+  }
+
+  if (!!localEntry) {
+    // Local entry takes precedence over db
+    finalEntry = localEntry;
+  }
+
+  // check for time precedence
+  if (!!databaseEntry && !!localEntry) {
+    if (databaseEntry["time"] && !localEntry["time"]) {
+      finalEntry = databaseEntry;
+    } else if (localEntry["time"] && !databaseEntry["time"]) {
+      finalEntry = localEntry;
+    } else if (databaseEntry["time"] && localEntry["time"]) {
+      if (databaseEntry["time"] > localEntry["time"]) {
+        finalEntry = databaseEntry;
+      } else {
+        finalEntry = localEntry;
+      }
+    }
+  }
+
+  if (!!databaseEntry && databaseEntry["label"] == "transphobe" && !!localEntry && localEntry["label"] == "local-transphobe") {
+    // Report was accepted
+    finalEntry = databaseEntry;
+  }
+  if (!!databaseEntry && databaseEntry["label"] == "appealed" && !!localEntry && localEntry["label"] == "local-appeal") {
+    // Appeal was accepted
+    finalEntry = databaseEntry;
+  }
+
+  if (finalEntry && finalEntry["label"] === "appealed") {
+    return null;
+  }
+  
+  return finalEntry;
+}
